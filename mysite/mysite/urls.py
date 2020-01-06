096cf8a4457c6d21223026236406a9f729e39311
ftcRibeiro <felipe.coltec@gmail.com>
2020-01-06 17:54:39 -0300

    app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from projectApp.core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
+    path('contact/',views.contact, name='contact'),
 ]
